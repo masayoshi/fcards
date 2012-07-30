104a146145e40b8e274b9d11c4edb1abbd1106d3
masay0shi <masayoshi.cdm@gmail.com>
2012-07-29 17:17:57 -0700

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# fcards
+
+This is the web service which helps you memorize things! by [Masayoshi](http://www.curiosity-drives.me/)
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Fcards::Application.config.secret_token = 'aa50ac4b76f7f73ceb367fa96b7c8f19152a590cd65a52d528aba3ee1a26022313d7e1745a96b1e74ea2a98341f7011ce046447d86960a905991001653b99c44'
